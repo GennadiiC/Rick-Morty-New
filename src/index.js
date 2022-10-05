675065bb36111a4c91810a27a3e10186007e0aa2
Gennadii Chamorsov <g@MBP-Gennadii.pautina.ua>
2022-10-05 21:56:38 +0300

    2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import { store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import './sass/custom.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
